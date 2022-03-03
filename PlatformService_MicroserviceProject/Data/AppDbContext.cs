09605c0a6b88eaf45ebf9b8fe1e3b806bfa2c926
mrgucci1 <dalquist.keaton221@gmail.com>
2022-03-02 23:06:37 -0600

    Add project files.

---
+++ PlatformService_MicroserviceProject/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService_MicroserviceProject.Models;
+namespace PlatformService_MicroserviceProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
---
+++ PlatformService_MicroserviceProject/Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+﻿using PlatformService_MicroserviceProject.Models;
+
+namespace PlatformService_MicroserviceProject.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
+    }
+}
