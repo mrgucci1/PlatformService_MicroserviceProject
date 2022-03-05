0c8d31951885d434e5d46b5c6ddad099407d841a
mrgucci1 <dalquist.keaton221@gmail.com>
2022-03-04 22:36:39 -0600

    Added 2 more API routes to our controller Working on getting docker to work, can run in a container but unable to push to docker hub yet

--- PlatformService_MicroserviceProject/Controllers/PlatformsController.cs
+++ PlatformService_MicroserviceProject/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService_MicroserviceProject.Data;
 using PlatformService_MicroserviceProject.Dtos;
+using PlatformService_MicroserviceProject.Models;
 
 namespace PlatformService_MicroserviceProject.Controllers
 {
@@ -25,6 +26,26 @@ namespace PlatformService_MicroserviceProject.Controllers
             var platformItems = _repository.GetAllPlatforms();
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
 
+        }
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("Getting Platform by id");
+            var platformItem = _repository.GetPlatformById(id);
+            if(platformItem == null)
+                return NotFound();
+            return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDtos platformCreateDtos)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDtos);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            //respone with code 201, say what request can be used to GET created platform, then respond with id and plaformreaddto of created object
+            //big part of REST
+            return CreatedAtRoute(nameof(GetPlatformById), new { id = platformReadDto.Id }, platformReadDto);
         }
     }
 }
