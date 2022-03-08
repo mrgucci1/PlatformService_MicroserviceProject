5b47d9eb63e0f470f4f46c88b55cb2fe00c31b1a
mrgucci1 <dalquist.keaton221@gmail.com>
2022-03-07 22:43:34 -0600

    Added some K8S deployment files, got our pod of the platform service up and running and our node port pod. Started on the command service implementing and testing a bare bones controller.

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+        [HttpPost]
+        public ActionResult TestInBoundConnection()
+        {
+            Console.WriteLine("Inbound POST # Command service");
+            return Ok("Inbound test okay from Platforms Controller");
+        }
+    }
+}
